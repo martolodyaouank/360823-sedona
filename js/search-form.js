5dc0bc6ad89baa490de428b37b6067fbbfd864d8
martolodyaouank <hyuehdgiwc@gmail.com>
2017-05-06 15:42:33 +0300

    Доработки

--- js/search-form.js
+++ js/search-form.js
@@ -1,16 +1,41 @@
 var link = document.querySelector(".reservation-search");
 var popup = document.querySelector(".form-description");
-var open = popup.querySelector(".reservation-search");
-var date = popup.querySelector("[name=date]");
+var dateIn = popup.querySelector("#date-in");
+var dateOut = popup.querySelector("#date-out");
+var adults = popup.querySelector("#amount-adults");
+var children = popup.querySelector("#amount-children");
+var submit = popup.querySelector(".search-btn");
 
-  link.addEventListener("click", function(event){
-    event.preventDefault();
-    if (popup.classList.contains("form-description-hide"))  {
-      popup.classList.add("form-description-show");
-      popup.classList.remove("form-description-hide");
-    } else {
-      popup.classList.add("form-description-hide");
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (popup.classList.contains("form-description-show")) {
+    popup.classList.add("form-description-hide");
+    popup.classList.remove("form-description-show");
+    popup.classList.remove("form-description-error");
+  } else {
+    popup.classList.add("form-description-show");
+    popup.classList.remove("form-description-hide");
+  };
+  dateIn.focus();
+});
+
+
+submit.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (!dateIn.value || !dateOut.value || !adults.value || !children.value) {
+    popup.classList.remove("form-description-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("form-description-error");
+  }
+});
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("form-description-show")) {
       popup.classList.remove("form-description-show");
-    };
-    date.focus();
-  });
+      popup.classList.add("form-description-hide");
+      popup.classList.remove("form-description-error");
+    }
+  }
+});
